2689fd4af7720e75b0981468c2e0d5e2593ce4cc
levxn <mslevin.daily@gmail.com>
2024-03-02 21:37:56 +0530

    update
    first commit

---
+++ script/train_denoise.sh
@@ -0,0 +1,1 @@
+python3 ./train/train_denoise.py --arch Uformer_B --batch_size 8 --gpu '0,1' --train_ps 128 --train_dir ./target/train --env _0706 --save_dir ./logs/
---
+++ utils/loader.py
@@ -0,0 +1,21 @@
+import os
+
+from dataset.dataset_denoise import DataLoaderTrain, DataLoaderVal, DataLoaderTest
+def get_training_data(rgb_dir, img_options):
+    print(rgb_dir)
+    assert os.path.exists(rgb_dir)
+    return DataLoaderTrain(rgb_dir, img_options, None)
+
+def get_validation_data(rgb_dir):
+    assert os.path.exists(rgb_dir)
+    return DataLoaderVal(rgb_dir, None)
+
+
+def get_test_data(rgb_dir):
+    assert os.path.exists(rgb_dir)
+    return DataLoaderTest(rgb_dir, None)
+
+
+# def get_test_data_SR(rgb_dir):
+#     assert os.path.exists(rgb_dir)
+#     return DataLoaderTestSR(rgb_dir, None)
